9c5fcdced91f39178541a9f25138d3ea45418be3
Jesse Molina <jesse@jessemolina.xyz>
2025-06-30 13:55:46 -0500

    Renamed dots to configs.

--- nvim/.config/nvim/lua/plugins/ui/dashboard-nvim.lua
+++ nvim/.config/nvim/lua/plugins/ui/dashboard-nvim.lua
@@ -16,7 +16,7 @@ return {
           "                                   ",
         },
         center = {
-          { desc = "Dots", key = "d", action = "lua vim.cmd('cd ~/lab/github/jessemolina/dotfiles'); require('telescope.builtin').find_files()" },
+          { desc = "Configs", key = "c", action = "lua vim.cmd('cd ~/lab/github/jessemolina/dotfiles'); require('telescope.builtin').find_files()" },
           { desc = "Files", key = "f", action = "Telescope find_files" },
           { desc = "Lab", key = "l", action = "lua require('telescope.builtin').find_files({cwd='~/lab/github/jessemolina', find_command={'find', '.', '-maxdepth', '1', '-type', 'd', '!', '-path', '.', '-printf', '%P\\n'}, attach_mappings=function(_, map) map('i', '<CR>', function(prompt_bufnr) local selection = require('telescope.actions.state').get_selected_entry() require('telescope.actions').close(prompt_bufnr) local dir = '~/lab/github/jessemolina/' .. selection.value vim.cmd('cd ' .. dir) require('telescope.builtin').find_files() end) return true end})" },
           { desc = "Notes", key = "n", action = "ObsidianToday" },
