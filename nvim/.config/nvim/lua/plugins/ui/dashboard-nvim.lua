a1ad9eb2a6700c707018f5299d57cd9f4353b9ff
Jesse Molina <jesse@jessemolina.xyz>
2025-06-30 09:21:30 -0500

    Updated dashboard center shortcuts.

--- nvim/.config/nvim/lua/plugins/ui/dashboard-nvim.lua
+++ nvim/.config/nvim/lua/plugins/ui/dashboard-nvim.lua
@@ -16,13 +16,13 @@ return {
           "                                   ",
         },
         center = {
-          { desc = "Dotfiles", key = "d", action = "lua vim.cmd('cd ~/lab/github/jessemolina/dotfiles'); require('telescope.builtin').find_files()" },
+          { desc = "Dots", key = "d", action = "lua vim.cmd('cd ~/lab/github/jessemolina/dotfiles'); require('telescope.builtin').find_files()" },
           { desc = "Files", key = "f", action = "Telescope find_files" },
           { desc = "Lab", key = "l", action = "lua require('telescope.builtin').find_files({cwd='~/lab/github/jessemolina', find_command={'find', '.', '-maxdepth', '1', '-type', 'd', '!', '-path', '.', '-printf', '%P\\n'}, attach_mappings=function(_, map) map('i', '<CR>', function(prompt_bufnr) local selection = require('telescope.actions.state').get_selected_entry() require('telescope.actions').close(prompt_bufnr) local dir = '~/lab/github/jessemolina/' .. selection.value vim.cmd('cd ' .. dir) require('telescope.builtin').find_files() end) return true end})" },
-          { desc = "Mentauro", key = "m", action = "lua require('telescope.builtin').find_files({cwd='~/lab/github/mentaurolabs', find_command={'find', '.', '-maxdepth', '1', '-type', 'd', '!', '-path', '.', '-printf', '%P\\n'}, attach_mappings=function(_, map) map('i', '<CR>', function(prompt_bufnr) local selection = require('telescope.actions.state').get_selected_entry() require('telescope.actions').close(prompt_bufnr) local dir = '~/lab/github/mentaurolabs/' .. selection.value vim.cmd('cd ' .. dir) require('telescope.builtin').find_files() end) return true end})" },
-          { desc = "Obsidian", key = "o", action = "ObsidianToday" },
+          { desc = "Notes", key = "o", action = "ObsidianToday" },
           { desc = "Recent", key = "r", action = "Telescope oldfiles" },
           { desc = "Update", key = "u", action = "Lazy update" },
+          { desc = "Work", key = "w", action = "lua require('telescope.builtin').find_files({cwd='~/lab/github/mentaurolabs', find_command={'find', '.', '-maxdepth', '1', '-type', 'd', '!', '-path', '.', '-printf', '%P\\n'}, attach_mappings=function(_, map) map('i', '<CR>', function(prompt_bufnr) local selection = require('telescope.actions.state').get_selected_entry() require('telescope.actions').close(prompt_bufnr) local dir = '~/lab/github/mentaurolabs/' .. selection.value vim.cmd('cd ' .. dir) require('telescope.builtin').find_files() end) return true end})" },
         },
         footer = function()
           local quotes = {
